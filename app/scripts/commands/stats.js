0cef514792956b54b8b82efdf75e44deb1fc0680
Pablo Novas <pjnovas@gmail.com>
2016-08-27 23:46:14 -0300

    added table ascii and a nicer stats

--- app/scripts/commands/stats.js
+++ app/scripts/commands/stats.js
@@ -1,14 +1,35 @@
 
+import AsciiTable from 'ascii-table';
 import store from '../store';
 
 export default {
   help: 'use stats to see your stats',
   run: function() {
     let state = store.getState();
-    this.echo(`- ${state.slaves.count} Slaves`);
-    this.echo('- Resources:');
-    this.echo(`  - ${state.resources.water} lts of water`);
-    this.echo(`  - ${state.resources.stone} kg of stone`);
-    this.echo(`  - ${state.resources.sand} kg of sand`);
+
+    var slTab = new AsciiTable()
+    slTab
+      .setBorder('=')
+      .addRow(
+        AsciiTable.align(AsciiTable.CENTER, state.slaves.count, 10),
+        AsciiTable.align(AsciiTable.CENTER, 'SLAVES', 23));
+
+    let resTab = new AsciiTable('Resources');
+
+    resTab
+      .setHeading(
+        AsciiTable.align(AsciiTable.CENTER, 'Water', 10),
+        AsciiTable.align(AsciiTable.CENTER, 'Stone', 10),
+        AsciiTable.align(AsciiTable.CENTER, 'Sand', 10))
+      .addRow(
+        `${state.resources.water} lt`,
+        `${state.resources.stone} kg`,
+        `${state.resources.sand} kg`)
+      .setAlignRight(0)
+      .setAlignRight(1)
+      .setAlignRight(2);
+
+    this.echo(slTab.toString())
+    this.echo(resTab.toString());
   }
 }
