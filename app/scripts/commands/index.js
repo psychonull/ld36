d07845448a4d88ffac70cc630c76cb65470a969b
Pablo Novas <pjnovas@gmail.com>
2016-08-28 13:13:27 -0300

    refactor commander

--- app/scripts/commands/index.js
+++ app/scripts/commands/index.js
@@ -7,6 +7,7 @@ import travel from './travel';
 import addresource from './addresource';
 import delresource from './delresource';
 import programs from './programs';
+import terms from './terms';
 
 exports.run = commander({
   born,
@@ -15,5 +16,6 @@ exports.run = commander({
   travel,
   addresource,
   delresource,
-  programs
+  programs,
+  terms
 });
