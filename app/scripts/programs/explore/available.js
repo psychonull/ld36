2a3443934d88918231b1337979de6698d52d5dee
Matias Arriola <matias.arriola@gmail.com>
2016-08-28 15:13:17 -0300

    add chance dep + initial terrains

--- app/scripts/programs/explore/available.js
+++ app/scripts/programs/explore/available.js
@@ -1,5 +1,24 @@
 import store from '../../store';
 
+const getDistance = (d) => {
+  return [
+    undefined,
+    'near',
+    'middle',
+    'far',
+    'far far far away'
+  ][d] || 'N/A';
+};
+
+const getRisk = (r) => {
+  return [
+    undefined,
+    'low',
+    'medium',
+    'high'
+  ][r] || 'N/A';
+};
+
 export default {
   help: 'list available terrains to explore',
   run: function(test) {
@@ -9,7 +28,11 @@ export default {
       this.error('NO MORE TERRAINS TO EXPLORE. THE KNOWN WORLD IS FULL OF YOUR CRAZY STUFF');
     }
     else {
-      this.echo(state.explorations.terrains.map((e,i) => 'campaign ' + i).join('\n'));
+      this.echo(state.explorations.terrains.map((t, i) =>
+        `${i + 1} - Direction: ${t.direction}
+    Distance:${getDistance(t.distance)}
+    Risk: ${getRisk(t.risk)}`
+      ).join('\n'));
     }
   }
 }
--- app/scripts/reducers/explorations.js
+++ app/scripts/reducers/explorations.js
@@ -1,10 +1,26 @@
+import Chance from 'chance';
+const chance = new Chance();
+
+chance.mixin({
+  'terrain': () => ({
+    direction: chance.pickone(['W','N','E','S']),
+    distance: chance.integer({min: 1, max: 4}),
+    risk: chance.integer({min: 1, max: 3})
+  })
+});
+
+const getInitialTerrains = () => {
+  var amount = chance.integer({min: 2, max: 4});
+  return chance.n(chance.terrain, amount);
+};
+
 const initialState = {
   current: [],
   recent: [],
-  terrains: []
+  terrains: getInitialTerrains()
 };
 
 export default function(state = initialState, action) {
-  
+
   return state;
 }
